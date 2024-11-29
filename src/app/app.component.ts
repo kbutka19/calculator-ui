46f157d92dae0b266a44f01ad495c2f5e7ead13d
kbutka19 <kejsibutka@yahoo.com>
2024-11-29 21:24:59 +0100

    Implemented section A in the utils file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { prodCubes, prodCubes2, prodFactorial, prodFactorial2, prodSquares, prodSquares2,  sumCubes, sumCubes2, sumFactorial, sumInt2, sumSquares, sumSquares2 } from './extras/utils';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,18 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'calculator-ui';
+
+  ngOnInit() {
+    console.log('normal',
+      sumSquares(1, 5),
+      sumCubes(1, 5), sumFactorial(1, 5)
+    )
+    console.log('formated', sumInt2(1, 5), sumSquares2(1, 5), sumCubes2(1, 5), sumFactorial(1, 5))
+    console.log('production', prodSquares(1, 5), prodCubes(1, 5), prodFactorial(1, 5))
+    console.log('production refactor', prodSquares2(1, 5), prodCubes2(1, 5), prodFactorial2(1, 5))
+    // console.log(prodSquares3(1, 5))
+  }
+
 }
